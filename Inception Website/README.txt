06f92cd2d092f7238fc00c1ae8172970bf40bd3f
Rajbir Deol <34774837+rajbird@users.noreply.github.com>
2023-05-03 11:09:03 -0400

    project load
    project load

---
+++ Inception Website/README.txt
@@ -0,0 +1,63 @@
+
+*****************************************************
+Rajbir Deol     0918139
+CIS*4300        Final Project
+18/12/2020
+*****************************************************
+
+Hello friends and welcome to DreamX. Here at DreamX
+we make your dreams come true. With our experienced
+personal we are able to crack anyone's mind to find
+out their secrets.
+
+
+*****************************************************
+Run Website
+*****************************************************
+
+To Experience the security version:
+1. Run start.html
+2. click on cat image --> get directed to loginM.html
+3. Click on sign in button
+4. Land on index.html
+
+To avoid security simply run index.html
+
+To get to Inception
+    - Try to find on your own, but if you give up scroll to bottom for hints
+
+    - To avoid above, simply run inception.html
+
+
+*****************************************************
+Image/Video References
+*****************************************************
+Home
+- https://wallpaperaccess.com/inception
+- https://tenor.com/search/inception-gifs
+About Us
+- https://www.youtube.com/watch?v=9U7_NBIHsQk
+Dream World Clipart
+- http://clipart-library.com/
+Inception
+- https://www.youtube.com/watch?v=Jvurpf91omw&feature=emb_title
+
+
+
+*****************************************************
+References
+*****************************************************
+All pertinent code References are located in the each file
+
+All data was siphoned from the inception wiki:
+https://inception.fandom.com/wiki/Inception_Wiki
+
+
+*****************************************************
+Finding the Inception Page
+*****************************************************
+1. Go to home page, or index.html
+2. Go to third slide on homepage
+3. Click transparent button underneath text
+4. Click let me in on next login page
+5. tada you found Inception
---
+++ Inception Website/assets/js/main.js
@@ -0,0 +1,108 @@
+!(function($) {
+  "use strict";
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  var scrolltoOffset = $('#header').outerHeight() - 17;
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top - scrolltoOffset;
+
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+
+
+  // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Intro carousel
+  var heroCarousel = $("#heroCarousel");
+  var heroCarouselIndicators = $("#hero-carousel-indicators");
+  heroCarousel.find(".carousel-inner").children(".carousel-item").each(function(index) {
+    (index === 0) ?
+    heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "' class='active'></li>"):
+      heroCarouselIndicators.append("<li data-target='#heroCarousel' data-slide-to='" + index + "'></li>");
+  });
+
+  heroCarousel.on('slid.bs.carousel', function(e) {
+    $(this).find('h2').addClass('animate__animated animate__fadeInDown');
+    $(this).find('p, .btn-get-started').addClass('animate__animated animate__fadeInUp');
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+
+
+  // Skills section
+  $('.skills-content').waypoint(function() {
+    $('.progress .progress-bar').each(function() {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+
+
+})(jQuery);
